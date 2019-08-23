1dcb27f6b8a7b333ac8b7552e7e104ec784626be
Amar Yassa <amaryassa@yahoo.fr>
2019-08-23 03:31:22 +0200

    fisrt commit/component/model

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Name', 'test description', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fitalian-turkey-orzo-soup-ck.jpg%3Fitok%3DQZCwMyYa&q=85')
+    ,new Recipe('Test Name', 'test description', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fitalian-turkey-orzo-soup-ck.jpg%3Fitok%3DQZCwMyYa&q=85')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
